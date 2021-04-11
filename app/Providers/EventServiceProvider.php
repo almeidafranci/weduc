e1a69e599192d0d1bfcc3f86679a8d6fbd487dd6
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2021-04-11 15:54:09 -0300

    Register event listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\ProgramCompiled;
+use App\Listeners\LogCompilation;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        ProgramCompiled::class => [
+            LogCompilation::class,
+        ]
     ];
 
     /**
