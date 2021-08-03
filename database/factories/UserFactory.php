489efdb388391cdc24fffe115096c44fb0e655d4
Julio Cesar <neto-vera@hotmail.com>
2021-08-03 15:10:30 -0300

    Remove test

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,20 +23,20 @@ class UserFactory extends Factory
    */
   public function definition()
   {
-    // return [
-    //   'name' => $this->faker->name,
-    //   'email' => $this->faker->unique()->safeEmail,
-    //   'email_verified_at' => now(),
-    //   'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-    //   'remember_token' => Str::random(10),
-    // ];
     return [
-      'name' => 'Dev Account',
-      'email' => 'dev@dev.com',
+      'name' => $this->faker->name,
+      'email' => $this->faker->unique()->safeEmail,
       'email_verified_at' => now(),
-      'password' => Hash::make('12345678'),
+      'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
       'remember_token' => Str::random(10),
     ];
+    //return [
+    //   'name' => 'Dev Account',
+    //   'email' => 'dev@dev.com',
+    //   'email_verified_at' => now(),
+    //   'password' => Hash::make('12345678'),
+    //   'remember_token' => Str::random(10),
+    // ];
   }
 
   /**
