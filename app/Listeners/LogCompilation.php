85fc85ce3fff418ec2b94c522d817d9c766c8ab1
Julio Cesar Vera Neto <neto-vera@hotmailk.com>
2021-10-31 01:05:03 -0300

    fix

--- app/Listeners/LogCompilation.php
+++ app/Listeners/LogCompilation.php
@@ -9,29 +9,29 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 
 class LogCompilation
 {
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+  /**
+   * Create the event listener.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    //
+  }
 
-    /**
-     * Handle the event.
-     *
-     * @param  ProgramCompiled  $event
-     * @return void
-     */
-    public function handle(ProgramCompiled $event)
-    {
-        $log = new CompilationLog();
-        $log->user_id = auth()->id();
-        $log->program_id = $event->program->id;
-        $log->exception = $event->exception ? get_class($event->exception) : null;
-        $log->is_successful = $event->exception ? true : false;
-        $log->save();
-    }
+  /**
+   * Handle the event.
+   *
+   * @param  ProgramCompiled  $event
+   * @return void
+   */
+  public function handle(ProgramCompiled $event)
+  {
+    $log = new CompilationLog();
+    $log->user_id = auth()->id();
+    $log->program_id = $event->program->id;
+    $log->exception = $event->exception ? get_class($event->exception) : null;
+    $log->is_successful = $event->exception ? true : false;
+    $log->save();
+  }
 }
